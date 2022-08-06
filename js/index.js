208c86de83ec9198316e963ba25c3acb51c73803
nr <99275945+giterhaber@users.noreply.github.com>
2022-08-07 01:21:12 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,8 +12,13 @@ firebase.initializeApp({
   measurementId: "G-SV1CFC7N5Q",
 });
 
+//ELECTRON PRELOAD CONNECT TEST
+console.log('preloadtest')
+
+
 //this is the name of our db collection to store chat messages
-const BASIC_CHAT_DB = "chat";
+
+var BASIC_CHAT_DB = "test";
 
 let NAME = "User_" + Math.round(Math.random() * 1000);
 
@@ -25,9 +30,10 @@ const db = firebase.firestore(); //get a handle to firestore DB
 function addMessage2DOM(data, id) {
   let side = data.name == NAME ? "mine" : "theirs";
   let msg = `<div class="message ${side}">
-    <div class="message__name">${data.name}</div>
+  <div class="message__name">${data.name}</div>
     <div class="message__bubble">${data.content}</div>
   </div>`;
+  //    
 
   const allMessages = document.querySelector(".messages");
   allMessages.innerHTML += msg; //add message to the page
@@ -68,7 +74,33 @@ function addMessage2DB(data) {
     .add(data)
     .then(function (docRef) {
       //if add is successful
-      console.log("Document written with ID: " + docRef.id);
+      //console.log("Document written with ID: " + docRef.id);
+      //console.log(data.content)
+
+      if (data.content == '/pornhub') {
+        window.open("https://cn.pornhub.com",
+        "_blank",  "width=463, height=600"); 
+      }
+
+       if   (data.content == '/facebook') {
+          window.open("https://messenger.com",
+          "_blank", "width=463, height=600");
+      }
+
+      if   (data.content == '/private') {
+        window.open("./private.html",
+        "_blank", "left=500, width=463, height=600");
+    }
+
+    if   (data.content == '/tools') {
+      window.open("./tools.html",
+      "_blank", "x=400, y=0, width=900, height=600");
+  }
+
+//logout
+
+
+
     })
     .catch(function (error) {
       //if add fails
@@ -83,9 +115,19 @@ db.collection(BASIC_CHAT_DB)
   .onSnapshot((querySnapshot) => {
     //this methods reads data from DB and calls addMessage2DOM
     console.log("querySnapshot.docs.length: " + querySnapshot.docs.length);
+
     //querySnapshot.forEach((doc) => {  //if you want all
     querySnapshot.docChanges().forEach((change) => {
       //if you want only changes
       addMessage2DOM(change.doc.data(), change.doc.id);
+
+
     });
   });
+
+    // renderer process (mainWindow)
+//window.open('https://cn.pornhub.com/', '_blank', 'top=0,left=1,frame=false,nodeIntegration=no')
+// const childWindow = window.open('', 'modal')
+// childWindow.document.write('<h1>Hello</h1>')
+
+
