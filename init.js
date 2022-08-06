796ca8ca3ee73f6905ef80a8d06a9c3a24e3b646
nr <99275945+giterhaber@users.noreply.github.com>
2022-08-07 01:20:39 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,22 +1,133 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
+const path = require('path')
+
+Menu.setApplicationMenu(false)
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 463,
+    x: 0,
+    y: 1,
+    width: 350,
     height: 600,
     resizable: false,
-    autoHideMenuBar: true,
+    alwaysOnTop: true,
+    frame: true,
+    transparent: false,
+    show: false,
     webPreferences: {
-      devTools: false
+      preload: path.join(__dirname, './preload.js')
+    }
+    
+  })
+  const child = new BrowserWindow({
+    center: true,
+    parent: win, 
+    modal: true,
+    width: 200,
+    height: 170,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, './preload.js')
     }
   })
 
+  child.once('ready-to-show', () => {
+    child.show()
+  })
+
+  child.loadFile('status.html')
+
+//
+  win.once('ready-to-show', () => {
+    win.show()
+
+  })
+
+  //win.webContents.openDevTools()
 
   win.loadFile('index.html')
+  // win.setMenuBarVisibility(false)
+}
+
+
+//ready
+app.whenReady().then(() => {
+  createWindow()
+
+})
+
+
+
+//sign
+const sign = () => {
+  const win = new BrowserWindow({
+        width: 390,
+        height: 350,
+        frame: false,
+        fullscreenable: false,
+        alwaysOnTop: true,
+        show: false,
+          webPreferences: {
+          preload: path.join(__dirname, './preload.js')
+          }
+
+  });
+
+  // win.webContents.setWindowOpenHandler(({ url }) => {
+  //   if (url == 'https://cn.pornhub.com') {
+  //     return {
+  //       action: 'allow',
+  //       overrideBrowserWindowOptions: {
+  //         width: 390,
+  //         height: 350,
+  //         resizable: false,
+  //         frame: false,
+  //         fullscreenable: false,
+  //         backgroundColor: black,
+  //         alwaysOnTop: true,
+  //         show: false,
+  //         webPreferences: {
+  //           preload: path.join(__dirname, './preload.js')
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return { action: 'allow'}
+  // })
+
+
+  // win.once('ready-to-show', () => {
+  //   win.show()
+  // })
+  //win.webContents.openDevTools()
+  win.loadFile('sign.html')
+  win.setMenuBarVisibility(false)
+
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  sign()
+
+  app.on('activate', function () {
+
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
+
+// app.whenReady().then(() => {
+//   const test = new BrowserWindow();
+//   test.loadFile('tools.html').then(() => {
+//     const currentURL = win.webContents.getURL()
+//     console.log(currentURL);
+//   })
+// })
+
+// app.whenReady().then(() => {
+//   const wina = new BrowserWindow({ show: false })
+// wina.once('ready-to-show', () => {
+//   wina.show()
+// })
+
+// })
 
